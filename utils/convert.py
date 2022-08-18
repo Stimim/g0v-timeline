92bbbdc591f46507033eee6dbaa0b64111bb4b7b
Stimim Chen <106878+Stimim@users.noreply.github.com>
2022-08-18 23:31:36 +0800

    add data converter
    Convert the data from CSV to JSON

---
+++ utils/convert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# HOWTO:
+#
+# Download CSV from https://docs.google.com/spreadsheets/d/1UBWZTLHtjaleq5qAc03OGKMRGMknUmz9PnKob-iFIk4/edit#gid=1086308132
+# rename the CSV as input.csv
+# Run this script
+#   python coverter.py >../src/app/data/g0v-events.ts
+
+import csv
+import json
+
+def main():
+
+  with open('./input.csv', 'r') as f:
+    reader = csv.DictReader(f)
+    data = []
+    for row in reader:
+      data.append(row)
+    data.sort(key=lambda row: row['date'])
+    print('export const EVENTS = %s;' % json.dumps(data) )
+
+if __name__ == '__main__':
+  main()
