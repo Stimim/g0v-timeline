98a367eb0c390c1b196ae52c55cb192c74371b2b
Stimim Chen <106878+Stimim@users.noreply.github.com>
2022-12-17 10:22:20 +0800

    update cli_tools
    1. Use wget to download CSV automatically
    2. Update the data to include english columns

--- cli_tools/run.sh
+++ cli_tools/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+wget 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTVuIen0HdVAjbUEs7KppNZt0XRbFrq5Rhfvw9bu5iuwc6bIbolkvtpKfb9tp0OSQjRWULhOaJhHTSi/pub?gid=1086308132&single=true&output=csv' -O input.csv
 python3 convert.py input.csv >../src/app/data/g0v-events.ts
 
